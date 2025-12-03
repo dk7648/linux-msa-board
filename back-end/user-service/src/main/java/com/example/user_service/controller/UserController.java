dd65de6aa29a04a18dde816275df79cada7636a5
dk7648 <dk7648@korea.ac.kr>
2025-12-03 19:49:29 +0900

    feat: user-service 버그 수정

--- back-end/user-service/src/main/java/com/example/user_service/controller/UserController.java
+++ back-end/user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -10,19 +10,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Service
-public class UserService implements UserDetailsService {
+@Controller
+public class UserController implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
